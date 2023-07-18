059d3aa5400fd7010ca886edf2f9993d878d21ae
KomisarzRyba <antek.olesik@gmail.com>
2023-07-18 14:58:41 +0200

    feat: navbar banner

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,15 @@
 'use client';
 
-import { CrumpledPaperIcon } from '@radix-ui/react-icons';
+import { cn } from '@/lib/utils';
+import { CodeIcon, CrumpledPaperIcon } from '@radix-ui/react-icons';
+import { motion, useScroll } from 'framer-motion';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React, { useEffect } from 'react';
 import ScrollLink from './ScrollLink';
-import { motion, useScroll } from 'framer-motion';
-import { usePathname } from 'next/navigation';
+import { Alert, AlertDescription, AlertTitle } from './ui/alert';
 import { buttonVariants } from './ui/button';
-import { cn } from '@/lib/utils';
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover';
 
 const Navbar: React.FC = () => {
 	const pathname = usePathname();
@@ -28,7 +30,7 @@ const Navbar: React.FC = () => {
 					y: 0,
 				},
 				hidden: {
-					y: -100,
+					y: -120,
 				},
 			}}
 			animate={hidden ? 'hidden' : 'visible'}
@@ -41,6 +43,31 @@ const Navbar: React.FC = () => {
 						Antek
 					</p>
 				</Link>
+				<Popover>
+					<PopoverTrigger>
+						<Alert className='w-fit h-fit px-8 hover:bg-secondary text-left'>
+							<CodeIcon className='w-4 h-4 mr-2' />
+							<AlertTitle className='text-sm'>
+								I&apos;m working on it!
+							</AlertTitle>
+							<AlertDescription className='text-xs'>
+								This site is under construction.
+							</AlertDescription>
+						</Alert>
+					</PopoverTrigger>
+					<PopoverContent>
+						<p className='text-sm'>
+							You can check out my latest version of this website{' '}
+							<a
+								href='https://antek.dev'
+								className='underline font-medium'
+							>
+								here
+							</a>
+							{'.'}
+						</p>
+					</PopoverContent>
+				</Popover>
 				<div className='flex items-center w-fit gap-6 justify-between'>
 					{pathname === '/' ? (
 						<ScrollLink targetId='about'>about</ScrollLink>
